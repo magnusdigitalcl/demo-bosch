f6ea23f78ae024fdb1917d0a8c98b8a06c57d02f
magnusdigitalcl <aopazo@magnusdigital.cl>
2025-12-17 22:13:04 -0300

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,14 @@
+function login() {
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+
+  if (user === "demo" && pass === "zaqxsw123") {
+    window.location.href = "checkout.html";
+  } else {
+    alert("Credenciales inválidas");
+  }
+}
+
+function logout() {
+  window.location.href = "login.html";
+}
